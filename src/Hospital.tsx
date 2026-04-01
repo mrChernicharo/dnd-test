577cbab47a26022a49144bc6f6f0926ba596a821
Felipe Melo Chernicharo <felipe.melo_chernicharo@genesys.com>
2026-04-01 15:40:28 -0300

    all looking good

--- src/Hospital.tsx
+++ src/Hospital.tsx
@@ -1,4 +1,11 @@
-import { useContainerWidth, type GridLayoutProps } from "react-grid-layout";
+import {
+  ReactGridLayout,
+  useContainerWidth,
+  type GridLayoutProps,
+  type LayoutItem,
+} from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 import "./Hospital.css";
 
 interface Room {
@@ -26,6 +33,11 @@ const hospital = {
         { id: "r102", name: "sala 102" },
         { id: "r103", name: "sala 103" },
         { id: "r104", name: "sala 104" },
+        { id: "r105", name: "sala 105" },
+        { id: "r106", name: "sala 106" },
+        { id: "r107", name: "sala 107" },
+        { id: "r108", name: "sala 108" },
+        { id: "r109", name: "sala 109" },
       ],
     },
     {
@@ -44,41 +56,81 @@ const hospital = {
 export function Hospital() {
   return (
     <div className="hospital">
-      {hospital.name}
+      <h1>{hospital.name}</h1>
 
-      <ul>
-        {hospital.sectors.map((sector) => (
-          <Sector key={sector.id} sector={sector} />
-        ))}
-      </ul>
+      {hospital.sectors.map((sector) => (
+        <Sector key={sector.id} sector={sector} />
+      ))}
     </div>
   );
 }
 
 export function Sector({ sector }: { sector: Sector }) {
-  //   const { width, mounted, containerRef } = useContainerWidth();
+  const { width, mounted, containerRef } = useContainerWidth({
+    measureBeforeMount: true,
+    // initialWidth: window.innerWidth,
+  });
 
-  //   const layout = buildSectorLayout(sector.rooms);
+  const cols = 12;
+  const sectorLayout = buildSectorLayout(sector.rooms, cols);
+  //   console.log({ sectorLayout });
 
   return (
     <div className="sector">
-      {sector.name}
-      <ul>
-        {sector.rooms.map((room) => (
-          <Room key={room.id} room={room} />
-        ))}
-      </ul>
+      <h2>{sector.name}</h2>
+
+      <div ref={containerRef}>
+        {mounted && (
+          <ReactGridLayout
+            layout={sectorLayout}
+            width={width}
+            gridConfig={{ cols, rowHeight: 40 }}
+            onLayoutChange={(ev) => console.log(ev)}
+          >
+            {sectorLayout?.map((item) => (
+              <div key={item.i}>
+                <Room room={item.room} />
+              </div>
+            ))}
+          </ReactGridLayout>
+        )}
+      </div>
     </div>
   );
 }
 
 export function Room({ room }: { room: Room }) {
-  return <li className="room">{room.name}</li>;
+  return <div className="room">{room.name}</div>;
 }
 
-// function buildSectorLayout(rooms: Room[]) {
-//   const layout: GridLayoutProps["layout"] = [];
+function buildSectorLayout(rooms: Room[], cols: number, width = 3, height = 2) {
+  const layout: (LayoutItem & { room: Room })[] = [];
 
-//   for (const room of rooms) {
-//   }
-// }
+  let y = 0;
+  let x = 0;
+
+  for (const room of rooms) {
+    // const isLong = Math.random() > 0.75;
+    // const isTall = Math.random() < 0.25;
+
+    const item: LayoutItem & { room: Room } = {
+      i: room.id,
+      x,
+      y,
+      //   w: isLong ? width * 2 : width,
+      //   h: isTall ? height * 2 : height,
+      w: width,
+      h: height,
+      isResizable: false,
+      isBounded: true,
+      room,
+    };
+
+    if (x + width >= cols) y++;
+    x = (x + width) % cols;
+
+    layout.push(item);
+  }
+
+  return layout;
+}
